dcfa31848808b59d64651e7c7c38f94abba04b7f
hs-mxmxm <d.perezmoreno@outlook.es>
2020-03-12 02:01:50 +0100

    RabbitMQ docker-compose volume

--- rabbitMQ_api/consumer/src/consumer.py
+++ rabbitMQ_api/consumer/src/consumer.py
@@ -1,17 +1,82 @@
 import pika
+import time
+import os
+from datetime import datetime
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='consumers')
+
+# RabbitMQ exchange
+EXCHANGE = 'my_exchange'
+
+# Name of rabbitMQ queue
+QUEUE = 'my_queue'
+
+# Log file
+LOG_FILE = str(datetime.date(datetime.now())) + ".txt"
+
+def main():
+    # Location of ARabbitMQ server from AMQP_URL variable
+    amqp_url = os.environ['AMQP_URL']
+    print('URL: %s' % (amqp_url))
+
+    # Stablish connection
+    parameters = pika.URLParameters(amqp_url)
+    connection = pika.BlockingConnection(parameters) # Connect to CloudAMQP
+    try:
+        opened_connection(connection) 
+
+    except KeyboardInterrupt:
+        print("[-] Quitting pruducer ...")
+        connection.close()
+
+
+def opened_connection(connection):
+    # Connection stablished
+    print("[!] Starting channel...")
+    channel = connection.channel()
+    opened_qos(channel,connection)
+
+
+def opened_qos(channel, connection):
+    print("[+] Queue QoS...")
+    channel.basic_qos(prefetch_count=1, 
+    callback=opened_queue(channel,connection))
+
+def opened_queue(channel, connection):
+    print("[+] Queue opened...")
+    channel.queue_bind(queue=QUEUE, exchange= '', routing_key=QUEUE, callback=open_message(channel, connection))
 
 def callback(ch, method, properties, body):
-    print(" Message received: %r" % body)
+    print("Callback")
+    read_message(body)
 
-channel.basic_consume(queue='consumers',
-                      auto_ack=True,
-                      on_message_callback=callback)
+def open_message(channel, connection):
+    print("Opening messages...")
+    channel.basic_consume(queue=QUEUE, on_message_callback=callback, auto_ack=True)
+    print("Waiting for messages...")
+    channel.start_consuming()
 
 
-print(' Waiting for message...')
-channel.start_consuming()
+def read_message(msg):
+    print("[+] Reading messages: \n")
+    msg = msg.decode("utf-8")
+    print("Message: %r " % msg)
+    saveFile(LOG_FILE, msg)
+    time.sleep(1)
+    return
+
+
+def saveFile(logfile, msg):
+    msg = "[" + str(datetime.time(datetime.now())) + "]: " + msg
+    try:
+        my_dir = os.path.dirname(os.path.relpath(__file__))
+        file = open(my_dir + "/" + logfile, "a")
+        file.write("\n" + msg)
+        file.close()
+    except IOError:
+        file = open(logfile, "w")
+        file.write("\n" + msg)
+        file.close()
+
+
+if __name__ == '__main__':
+    main()
--- rabbitMQ_api/producer/src/producer.py
+++ rabbitMQ_api/producer/src/producer.py
@@ -1,14 +1,50 @@
 import pika
 import time
+import os
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='consumers')
-while True:
-    channel.basic_publish(exchange='',
-                        routing_key='consumers', 
-                        body='hello World!')
-    print('Message sent.')
+# RabbitMQ exchange
+# EXCHANGE = 'my_exchange'
+
+# AMQP routing key when sending a message
+ROUTING_KEY = 'consumers_queue'
+
+# AMQP queue
+QUEUE = 'my_queue'
+
+# Delay between messages
+DELAY = 0.1
+
+def main():
+    # Location of ARabbitMQ server from AMQP_URL variable
+    amqp_url = os.environ['AMQP_URL']
+    print('URL: %s' % (amqp_url))
+
+    # Stablish connection
+    parameters = pika.URLParameters(amqp_url)
+    parameters.socket_timeout = 5
+    connection = pika.BlockingConnection(parameters) # Connect to CloudAMQP
+    try:
+        opened_connection(connection) 
+
+    except KeyboardInterrupt:
+        print("[-] Quitting pruducer ...")
+        connection.close()
+
+
+def opened_connection(connection):
+    # Connection stablished
+    print("[!] Starting channel...")
+    channel = connection.channel()
+    send_message(channel, 0)
+
+
+def send_message(channel, i):
+    msg = 'Message %d' % (i)
+    channel.basic_publish(exchange='', routing_key=QUEUE, body=msg)
+    print("[*] Message published: {} ".format(msg))
     time.sleep(2)
-    
-connection.close()
+    send_message(channel, i+1)
+
+
+if __name__ == '__main__':
+    main()
