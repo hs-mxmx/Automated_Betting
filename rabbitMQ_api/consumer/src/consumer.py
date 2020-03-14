0641b197ba60ca5c4c68ee89443b766c843022ee
hs-mxmx <d.perezmoreno@outlook.es>
2020-03-14 12:38:20 +0100

    Flask automation & pandas tables

--- rabbitMQ_api/consumer/src/consumer.py
+++ rabbitMQ_api/consumer/src/consumer.py
@@ -1,8 +1,14 @@
 import pika
 import time
 import os
+import queue
+from flask import Flask
+from flask import render_template
 from datetime import datetime
+from threading import Thread
 
+# Create Flask instance
+app = Flask(__name__)
 
 # RabbitMQ exchange
 EXCHANGE = 'my_exchange'
@@ -13,9 +19,13 @@ QUEUE = 'my_queue'
 # Log file
 LOG_FILE = str(datetime.date(datetime.now())) + ".txt"
 
+# Python queue
+myData = []
+temp_data = []
+
 def main():
-    # Location of ARabbitMQ server from AMQP_URL variable
-    amqp_url = os.environ['AMQP_URL']
+    # Location of RabbitMQ server from AMQP_URL variable
+    amqp_url =  "amqp://xxalaqou:dQFGDDlp-pfhSolv57XHhVWeqmzcmD6l@crow.rmq.cloudamqp.com/xxalaqou"
     print('URL: %s' % (amqp_url))
 
     # Stablish connection
@@ -25,7 +35,7 @@ def main():
         opened_connection(connection) 
 
     except KeyboardInterrupt:
-        print("[-] Quitting pruducer ...")
+        print("[-] Quitting consumer ...")
         connection.close()
 
 
@@ -53,30 +63,60 @@ def open_message(channel, connection):
     print("Opening messages...")
     channel.basic_consume(queue=QUEUE, on_message_callback=callback, auto_ack=True)
     print("Waiting for messages...")
-    channel.start_consuming()
-
+    thread = Thread(channel.start_consuming())
+    thread.start()
 
 def read_message(msg):
-    print("[+] Reading messages: \n")
+#   print("[+] Reading messages: \n")
     msg = msg.decode("utf-8")
-    print("Message: %r " % msg)
+#   q.put(msg)
+#   for elem in q.queue: 
     saveFile(LOG_FILE, msg)
+    temp_message = read_tempFile("temp-2020-03-13.txt")
+    temp_data = temp_message
+    myData.append(msg)
+    print("Messages: %r " % temp_data)
     time.sleep(1)
     return
 
+def read_tempFile(logfile):
+    my_temp_list = []
+    with open(logfile, "r")as my_file: data = my_file.read().replace('\n', ' ')
+    my_temp_list = list(data.split(' '))
+    return my_temp_list
+
 
 def saveFile(logfile, msg):
+    temp_msg = msg
     msg = "[" + str(datetime.time(datetime.now())) + "]: " + msg
     try:
-        my_dir = os.path.dirname(os.path.relpath(__file__))
-        file = open(my_dir + "/" + logfile, "a")
+        file = open(logfile, "a")
         file.write("\n" + msg)
         file.close()
+        file_temp = open("temp-"+logfile, "a")
+        file_temp.write("\n" + temp_msg)
     except IOError:
         file = open(logfile, "w")
         file.write("\n" + msg)
         file.close()
+        file_temp = open("temp-"+logfile, "w")
+        file_temp.write("\n" + temp_msg)
+    finally:
+        file.close()
+        file_temp.close()
 
+@app.route('/')
+@app.route('/index')
+def index():
+    temp_message = read_tempFile("temp-2020-03-13.txt")
+    return render_template('index.html', title='Index', data=temp_message)
+
+@app.route('/home')
+def home():
+    return render_template('index.html', title='Home', data='Home')
 
 if __name__ == '__main__':
-    main()
+    thread = Thread(target=main)
+    thread.daemon = True
+    thread.start()
+    Thread(target=app.run(debug=True, host='0.0.0.0')).start()
---
+++ rabbitMQ_api/consumer/src/myCients_table.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template, session, redirect
+import numpy as np
+import pandas as pd
+
+
+app = Flask(__name__)
+
+myData = {"Name":'Telefonica', "Total Price":'1000', "Total Number": '2'}
+
+df = pd.DataFrame({'Name': [myData.get("Name",'')],
+                   'Total Price': [myData.get("Total Price",'')],
+                   'Total number': [myData.get("Total Number",'')]})
+
+
+@app.route('/', methods=("POST", "GET"))
+def html_table():
+
+    return render_template('table.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
